d7bd321a48f1e809c73594b7058d3c56c50838e2
Adi <u6051965@anu.edu.au>
2020-11-02 23:58:50 +1100

    testing with maps api nodes

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,44 @@ function initMap() {
     zoom: 16,
   });
 }
+
+// var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+// var mapOptions = {
+//   zoom: 4,
+//   center: myLatlng
+// }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+// var marker = new google.maps.Marker({
+//     position: myLatlng,
+//     title:"Hello World!"
+// });
+
+// // To add the marker to the map, call setMap();
+// marker.setMap(map);
+
+
+
+// function initMap() {
+//   const myLatlng = { lat: -25.363, lng: 131.044 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: myLatlng,
+//   });
+//   const marker = new google.maps.Marker({
+//     position: myLatlng,
+//     map,
+//     title: "Click to zoom",
+//   });
+//   map.addListener("center_changed", () => {
+//     // 3 seconds after the center of the map has changed, pan back to the
+//     // marker.
+//     window.setTimeout(() => {
+//       map.panTo(marker.getPosition());
+//     }, 3000);
+//   });
+//   marker.addListener("click", () => {
+//     map.setZoom(8);
+//     map.setCenter(marker.getPosition());
+//   });
+// }
